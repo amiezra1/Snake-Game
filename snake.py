f1c2245e1724bf01f2ddc67aa440c733cbc1b996
amiezra1 <myzr2425@gmail.com>
2024-07-05 13:40:57 +0300

    Ability to control the snake using the keyboard

--- snake.py
+++ snake.py
@@ -2,17 +2,26 @@ from turtle import Turtle
 
 STARTING_POSITIONS = [(0,0), (-20,0), (-40,0)]
 MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
 
 class Snake:
 
   def __init__(self) -> None:
     self.segments = []
     self.create_snake()
+    self.head = self.segments[0]
   
   def create_snake(self):
     for position in STARTING_POSITIONS:
       new_segment=Turtle("square")
-      new_segment.color("white")
+      if len(self.segments) == 0:
+        new_segment.color("green")
+      else:
+        new_segment.color("white")
       new_segment.penup()
       new_segment.goto(position)
       self.segments.append(new_segment)
@@ -22,4 +31,20 @@ class Snake:
       new_x = self.segments[segment_num - 1].xcor()
       new_y = self.segments[segment_num - 1].ycor()
       self.segments[segment_num].goto(new_x, new_y)
-    self.segments[0].forward(MOVE_DISTANCE)
+    self.head.forward(MOVE_DISTANCE)
+
+  def up(self):
+    if self.head.heading() != DOWN:
+      self.head.setheading(UP)
+  
+  def left(self):
+    if self.head.heading() != RIGHT:
+      self.head.setheading(LEFT)
+
+  def down(self):
+    if self.head.heading() != UP:
+      self.head.setheading(DOWN)
+
+  def right(self):
+    if self.head.heading() != LEFT:
+      self.head.setheading(RIGHT)
